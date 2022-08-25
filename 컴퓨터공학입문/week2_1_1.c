459596f0d89fbb06761c00b881ba3012467c8010
sypack1997 <sypack1997@naver.com>
2022-08-25 21:53:22 +0900

    upload_2

--- 컴퓨터공학입문/week2_1_1.c
+++ 컴퓨터공학입문/week2_1_1.c
@@ -1,5 +1,6 @@
 /* The traditional first program in honor of  Dennis Ritchie who invented C at Bell Labs in 1972 */
 
+/* 전처리기(preprocessor : 컴파일러가 프로그래밍 언어를 기계어로 바꾸기 전에 처리해야할 명령을 먼저 수행하는 것)*/
 
 #include <stdio.h> /* 표쥰 입출력 함수를 사용하기 위해 stdio.h 헤더 파일을 소스 코드에 포함 */
 
